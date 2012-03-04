4d96a802d453dc2c930570b9373c18258ce518e3
Kiran Jonnalagadda <jace@pobox.com>
2012-03-04 17:47:45 +0530

    Use #/ to prevent page scrolling on tab change.

--- baseframe/static/js/baseframe.js
+++ baseframe/static/js/baseframe.js
@@ -12,17 +12,17 @@ $(function() {
   // Load correct tab when fragment identifier changes
   $(window).bind('hashchange', function() {
     var url = document.location.toString();
-    if (url.match('#')) {
-      $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+    if (url.match('#/')) {
+      $('.nav-tabs a[href=#'+url.split('#/')[1]+']').tab('show') ;
     }
   });
   // Load correct tab when the page loads
   var url = document.location.toString();
-  if (url.match('#')) {
-    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+  if (url.match('#/')) {
+    $('.nav-tabs a[href=#'+url.split('#/')[1]+']').tab('show') ;
   }
   // Change hash for tab click
   $('.nav-tabs a').on('shown', function (e) {
-    window.location.hash = e.target.hash;
+    window.location.hash = '#/' + e.target.hash.slice(1);
   });
 });
