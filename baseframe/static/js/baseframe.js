33da5c36e01eccf816be4d2e3822a7b140b862b7
Kiran Jonnalagadda <jace@pobox.com>
2012-03-04 17:31:08 +0530

    Switching tabs updates location hash.

--- baseframe/static/js/baseframe.js
+++ baseframe/static/js/baseframe.js
@@ -8,4 +8,21 @@ function activate_chosen(selector) {
 
 $(function() {
   activate_chosen('select');
+
+  // Load correct tab when fragment identifier changes
+  $(window).bind('hashchange', function() {
+    var url = document.location.toString();
+    if (url.match('#')) {
+      $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+    }
+  });
+  // Load correct tab when the page loads
+  var url = document.location.toString();
+  if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+  }
+  // Change hash for tab click
+  $('.nav-tabs a').on('shown', function (e) {
+    window.location.hash = e.target.hash;
+  });
 });
