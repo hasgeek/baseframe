49b6a1c08fe6fb9669733123c87c64376466043a
Kiran Jonnalagadda <jace@pobox.com>
2012-06-15 03:32:55 +0530

    Support for multipart/form-data in forms with file fields.

--- baseframe/forms.py
+++ baseframe/forms.py
@@ -104,14 +104,18 @@ class ConfirmDeleteForm(Form):
 
 
 def render_form(form, title, message='', formid='form', submit=u"Submit", cancel_url=None, ajax=False):
+    multipart = False
+    for field in form:
+        if isinstance(field.widget, wtf.FileInput):
+            multipart = True
     if request.is_xhr and ajax:
         return render_template('baseframe/ajaxform.html', form=form, title=title,
             message=message, formid=formid, submit=submit,
-            cancel_url=cancel_url)
+            cancel_url=cancel_url, multipart=multipart)
     else:
         return render_template('baseframe/autoform.html', form=form, title=title,
             message=message, formid=formid, submit=submit,
-            cancel_url=cancel_url, ajax=ajax)
+            cancel_url=cancel_url, ajax=ajax, multipart=multipart)
 
 
 def render_message(title, message):
