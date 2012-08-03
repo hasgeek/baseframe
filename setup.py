c15b6a46d3b87ec6f267215e54b80a0e020f405d
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-08-04 03:43:10 +0530

    Additional blocks in the base template.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class BaseframeBuildPy(build_py):
 
 
 setup(name='baseframe',
-      version='0.2.9',
+      version='0.2.10',
       description='Baseframe for HasGeek projects',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
