abb12a55cb014bf5fcd6a6f8bbb5dc83e2ffe571
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-11-25 16:13:24 +0530

    Switch back from select2 to chosen.

--- baseframe/static/js/baseframe.js
+++ baseframe/static/js/baseframe.js
@@ -1,10 +1,10 @@
 // Activate chosen.js only if not on a mobile browser
 // This is a global function. Isn't there a better way to do this?
 function activate_chosen(selector) {
-  $(selector).select2({allowClear: true});
-  // if (!navigator.userAgent.match(/(iPod|iPad|iPhone|Android)/)) {
-  //   $(selector).chosen({allow_single_deselect: true});
-  // }
+  // $(selector).select2({allowClear: true});
+  if (!navigator.userAgent.match(/(iPod|iPad|iPhone|Android)/)) {
+    $(selector).chosen({allow_single_deselect: true});
+  }
 }
 
 $(function() {
