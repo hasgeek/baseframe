607d23c6a8c0e2a1d080bd0352d054b85bc3094d
Praseetha-KR <praseetha@hasgeek.com>
2013-08-20 15:06:15 +0530

    Switching between navbar-tggle buttons for mobile - refined

--- baseframe/static/js/networkbar.js
+++ baseframe/static/js/networkbar.js
@@ -19,18 +19,24 @@ $(function(){
 $(function(){
 	$('#navbar-toggle-1').click(function(){
 		if(!$('#navbar-toggle-2').hasClass('collapse')){
-			$('#navbar-toggle-2').addClass('collapsed');
-			$('#navbar-collapse-2').addClass('collapse');
-			$('#navbar-collapse-2').removeClass('in');
-			$('#navbar-collapse-2').css('height', '2px');
+			var collapse2 = function(){
+				$('#navbar-toggle-2').addClass('collapsed');
+				$('#navbar-collapse-2').addClass('collapse');			
+				$('#navbar-collapse-2').removeClass('in');	
+			}
+			setTimeout(collapse2, 480);			
+			$('#navbar-collapse-2').animate({height:"2px"});
 		}
 	});
 	$('#navbar-toggle-2').click(function(){
 		if(!$('#navbar-toggle-1').hasClass('collapse')){
-			$('#navbar-toggle-1').addClass('collapsed');
-			$('#navbar-collapse-1').addClass('collapse');
-			$('#navbar-collapse-1').removeClass('in');
-			$('#navbar-collapse-1').css('height', '2px');
+			var collapse1 = function(){
+				$('#navbar-toggle-1').addClass('collapsed');
+				$('#navbar-collapse-1').addClass('collapse');			
+				$('#navbar-collapse-1').removeClass('in');	
+			}
+			setTimeout(collapse1, 480);			
+			$('#navbar-collapse-1').animate({height:"2px"});
 		}
 	});
 });
