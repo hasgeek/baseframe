a6a96076fb582286dd99ea88d234592ed73374df
Praseetha-KR <praseetha@hasgeek.com>
2013-08-20 13:22:35 +0530

    Switching between navbar-tggle buttons for mobile - primitive method

--- baseframe/static/js/networkbar.js
+++ baseframe/static/js/networkbar.js
@@ -16,3 +16,21 @@ $(function(){
 		$('.navbar-collapse').css('max-height', 390);
 	}
 });
+$(function(){
+	$('#navbar-toggle-1').click(function(){
+		if(!$('#navbar-toggle-2').hasClass('collapse')){
+			$('#navbar-toggle-2').addClass('collapsed');
+			$('#navbar-collapse-2').addClass('collapse');
+			$('#navbar-collapse-2').removeClass('in');
+			$('#navbar-collapse-2').css('height', '2px');
+		}
+	});
+	$('#navbar-toggle-2').click(function(){
+		if(!$('#navbar-toggle-1').hasClass('collapse')){
+			$('#navbar-toggle-1').addClass('collapsed');
+			$('#navbar-collapse-1').addClass('collapse');
+			$('#navbar-collapse-1').removeClass('in');
+			$('#navbar-collapse-1').css('height', '2px');
+		}
+	});
+});
