a308a40c284a4e79cc9604ae4edd2353f4499b81
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-01-30 23:50:09 +0530

    Catch all exceptions but log it for later analysis.

--- baseframe/signals.py
+++ baseframe/signals.py
@@ -7,3 +7,4 @@ baseframe_signals = Namespace()
 
 form_validation_error = baseframe_signals.signal('form-validation-error')
 form_validation_success = baseframe_signals.signal('form-validation-success')
+exception_catchall = baseframe_signals.signal('exception-catchall')
