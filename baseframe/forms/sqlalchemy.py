587ffeefbcecf41a2b16094822b02f9e39652e32
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-10-02 12:46:40 +0530

    AvailableName validator takes model directly now.

--- baseframe/forms/sqlalchemy.py
+++ baseframe/forms/sqlalchemy.py
@@ -15,19 +15,20 @@ class AvailableName(object):
     Validator to check whether the specified name is available
     for the model being edited.
     """
-    def __init__(self, message=None, scoped=False):
+    def __init__(self, message=None, model=None, scoped=False):
+        self.model = model
         self.scoped = scoped
         if not message:
-            message = __("That URL name is already in use")
+            message = __("This URL name is already in use")
         self.message = message
 
     def __call__(self, form, field):
-        if form.edit_model:
-            query = form.edit_model.query.filter_by(name=field.data)
+        model = self.model or form.edit_model
+        if model:
+            query = model.query.filter_by(name=field.data)
             if form.edit_id:
-                query = query.filter(form.edit_model.id != form.edit_id)
+                query = query.filter(model.id != form.edit_id)
             if self.scoped:
                 query = query.filter_by(parent=form.edit_parent)
-            existing = query.count()
-            if existing:
+            if query.notempty():
                 raise wtforms.validators.StopValidation(self.message)
