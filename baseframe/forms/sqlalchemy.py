7784be44c5b66400d1bdb8612782ce6661baa991
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-12-03 05:07:55 +0530

    Automatic scoped model detection.

--- baseframe/forms/sqlalchemy.py
+++ baseframe/forms/sqlalchemy.py
@@ -15,20 +15,23 @@ class AvailableName(object):
     Validator to check whether the specified name is available
     for the model being edited.
     """
-    def __init__(self, message=None, model=None, scoped=False):
+    def __init__(self, message=None, model=None):
         self.model = model
-        self.scoped = scoped
         if not message:
             message = __("This URL name is already in use")
         self.message = message
 
     def __call__(self, form, field):
         model = self.model or form.edit_model
+        if hasattr(model, 'parent'):
+            scoped = True
+        else:
+            scoped = False
         if model:
             query = model.query.filter_by(name=field.data)
             if form.edit_id:
                 query = query.filter(model.id != form.edit_id)
-            if self.scoped:
+            if scoped:
                 query = query.filter_by(parent=form.edit_parent)
             if query.notempty():
                 raise wtforms.validators.StopValidation(self.message)
