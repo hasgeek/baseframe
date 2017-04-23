3f8647d596942d630d98d3911992dec177e3dae4
Shreyas Satish <shreyas.satish@gmail.com>
2017-04-23 23:08:15 +0530

    added AvailableAttr

--- baseframe/forms/sqlalchemy.py
+++ baseframe/forms/sqlalchemy.py
@@ -11,15 +11,16 @@ from .validators import StopValidation
 __all__ = ['AvailableName', 'QuerySelectField', 'QuerySelectMultipleField']
 
 
-class AvailableName(object):
+class AvailableAttr(object):
     """
-    Validator to check whether the specified name is available
+    Validator to check whether the specified attribute is available
     for the model being edited.
     """
-    def __init__(self, message=None, model=None):
+    def __init__(self, attr, message=None, model=None):
         self.model = model
+        self.attr = attr
         if not message:
-            message = __("This URL name is already in use")
+            message = __("This {attr} is already in use".format(attr=attr))
         self.message = message
 
     def __call__(self, form, field):
@@ -29,10 +30,21 @@ class AvailableName(object):
         else:
             scoped = False
         if model:
-            query = model.query.filter_by(name=field.data)
+            query = model.query.filter(getattr(model, self.attr) == field.data)
             if form.edit_id:
                 query = query.filter(model.id != form.edit_id)
             if scoped:
                 query = query.filter_by(parent=form.edit_parent)
             if query.notempty():
                 raise StopValidation(self.message)
+
+
+class AvailableName(AvailableAttr):
+    """
+    Validator to check whether the specified name is available
+    for the model being edited.
+    """
+    def __init__(self, message=None, model=None):
+        if not message:
+            message = __("This URL name is already in use")
+        super(AvailableName, self).__init__('name', message, model)
