f30f087999945cb10a557db34726517e8f443b35
Bibhas <me@bibhas.in>
2017-07-21 10:02:02 +0530

    Added unit tests for filters (#156)
    * added tests for filters
    
    * added test_requirements and modified travis config
    
    * added few more tests
    
    * added test for gravatar url
    
    * removed unnecessary tearDown method and added comment

---
+++ tests/fixtures.py
@@ -0,0 +1,21 @@
+import unittest
+from flask import Flask
+from baseframe import baseframe
+
+
+class BaseframeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        baseframe.init_app(self.app, requires=['baseframe'])
+
+
+class TestUser(object):
+    def __init__(self, avatar=None, email=None):
+        self.avatar = self.set_avatar(avatar)
+        self.email = self.set_email(email)
+
+    def set_avatar(self, avatar):
+        self.avatar = avatar
+
+    def set_email(self, email):
+        self.email = email
---
+++ tests/test_filters.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timedelta
+
+from coaster.utils import md5sum
+from baseframe import filters
+from .fixtures import BaseframeTestCase, TestUser
+
+
+class FilterTestCase(BaseframeTestCase):
+    def setUp(self):
+        super(FilterTestCase, self).setUp()
+        self.now = datetime.utcnow()
+        self.test_user = TestUser()
+        self.test_avatar_size = ('100', '100')
+        self.test_avatar_url = u'//images.hasgeek.com/embed/test'
+
+    def test_age(self):
+        age = filters.age(self.now)
+        self.assertEqual(age, u'now')
+
+        nine_seconds = self.now - timedelta(seconds=9)
+        age = filters.age(nine_seconds)
+        self.assertEqual(age, u'seconds ago')
+
+        thirty_minutes = self.now - timedelta(minutes=30)
+        age = filters.age(thirty_minutes)
+        self.assertEqual(age, u'30 minutes ago')
+
+        one_half_hour = self.now - timedelta(hours=1.5)
+        age = filters.age(one_half_hour)
+        self.assertEqual(age, u'an hour ago')
+
+        ten_months = self.now - timedelta(days=10*30)
+        age = filters.age(ten_months)
+        self.assertEqual(age, u'10 months ago')
+
+        three_years = self.now - timedelta(days=3*12*30)
+        age = filters.age(three_years)
+        self.assertEqual(age, u'2 years ago')
+
+    def test_usessl(self):
+        with self.app.test_request_context('/'):
+            self.app.config['USE_SSL'] = False
+            ssled = filters.usessl('http://hasgeek.com')
+            self.assertEqual(ssled, 'http://hasgeek.com')
+
+            self.app.config['USE_SSL'] = True
+            ssled = filters.usessl('http://hasgeek.com')
+            self.assertEqual(ssled, 'https://hasgeek.com')
+
+            ssled = filters.usessl('hasgeek.com')
+            self.assertEqual(ssled, 'hasgeek.com')
+
+            ssled = filters.usessl('/static/test')
+            self.assertEqual(ssled, 'https://localhost/static/test')
+
+    def test_nossl(self):
+        with self.app.test_request_context('/'):
+            nossled = filters.nossl('https://hasgeek.com')
+            self.assertEqual(nossled, 'http://hasgeek.com')
+
+            nossled = filters.nossl('hasgeek.com')
+            self.assertEqual(nossled, 'hasgeek.com')
+
+            nossled = filters.nossl('//hasgeek.com')
+            self.assertEqual(nossled, 'http://hasgeek.com')
+
+    def test_avatar_url(self):
+        # test_user object doesn't have an email or an avatar by default
+        avatar_url = filters.avatar_url(self.test_user)
+        self.assertEqual(avatar_url, u'//www.gravatar.com/avatar/00000000000000000000000000000000?d=mm')
+
+        # testing what if the user has an avatar already
+        self.test_user.set_avatar(self.test_avatar_url)
+        avatar_url = filters.avatar_url(self.test_user, self.test_avatar_size)
+        self.assertEqual(avatar_url, self.test_avatar_url + '?size=' + u'x'.join(self.test_avatar_size))
+
+        # what if the user doesn't have an avatar but has an email
+        self.test_user.set_avatar(None)
+        self.test_user.set_email('foobar@foo.com')
+        avatar_url = filters.avatar_url(self.test_user, self.test_avatar_size)
+        hash = md5sum(self.test_user.email)
+        self.assertEqual(avatar_url, u'//www.gravatar.com/avatar/' + hash + u'?d=mm&s=' + u'x'.join(self.test_avatar_size))
