33d0dbc367d90d168f3289351ce414bad4d082a9
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-10-06 14:32:25 +0530

    Fix indentation.

--- baseframe/__init__.py
+++ baseframe/__init__.py
@@ -44,13 +44,21 @@ DEFAULT_DOGPILE_CONFIG = {
 }
 
 THEME_FILES = {
-    'bootstrap3': {'ajaxform.html.jinja2': 'baseframe/bootstrap3/ajaxform.html.jinja2', 'autoform.html.jinja2': 'baseframe/bootstrap3/autoform.html.jinja2',
-        'delete.html.jinja2': 'baseframe/bootstrap3/delete.html.jinja2', 'message.html.jinja2': 'baseframe/bootstrap3/message.html.jinja2',
-        'redirect.html.jinja2': 'baseframe/bootstrap3/redirect.html.jinja2'},
-    'mui': {'ajaxform.html.jinja2': 'baseframe/mui/ajaxform.html.jinja2', 'autoform.html.jinja2': 'baseframe/mui/autoform.html.jinja2',
-        'delete.html.jinja2': 'baseframe/mui/delete.html.jinja2', 'message.html.jinja2': 'baseframe/mui/message.html.jinja2',
-        'redirect.html.jinja2': 'baseframe/mui/redirect.html.jinja2'}
-}
+    'bootstrap3': {
+        'ajaxform.html.jinja2': 'baseframe/bootstrap3/ajaxform.html.jinja2',
+        'autoform.html.jinja2': 'baseframe/bootstrap3/autoform.html.jinja2',
+        'delete.html.jinja2': 'baseframe/bootstrap3/delete.html.jinja2',
+        'message.html.jinja2': 'baseframe/bootstrap3/message.html.jinja2',
+        'redirect.html.jinja2': 'baseframe/bootstrap3/redirect.html.jinja2'
+        },
+    'mui': {
+        'ajaxform.html.jinja2': 'baseframe/mui/ajaxform.html.jinja2',
+        'autoform.html.jinja2': 'baseframe/mui/autoform.html.jinja2',
+        'delete.html.jinja2': 'baseframe/mui/delete.html.jinja2',
+        'message.html.jinja2': 'baseframe/mui/message.html.jinja2',
+        'redirect.html.jinja2': 'baseframe/mui/redirect.html.jinja2'
+        }
+    }
 
 baseframe_translations = Domain(translations.__path__[0], domain='baseframe')
 _ = baseframe_translations.gettext
