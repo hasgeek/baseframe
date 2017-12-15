a5f5c49b9a08985d9def7495d56af86d2f6c7bc4
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-12-15 17:28:33 +0530

    Remove delete_template.html

--- baseframe/forms/auto.py
+++ baseframe/forms/auto.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import wtforms
-from flask import render_template, request, Markup, abort, flash, redirect, escape, url_for, make_response, current_app, jsonify
+from flask import render_template, request, Markup, abort, flash, redirect, escape, url_for, make_response, current_app
 from .. import b__ as __
-from .. import b_ as _
 from .. import THEME_FILES
 from .form import Form
 from .fields import SubmitField
@@ -23,11 +22,6 @@ def render_form(form, title, message='', formid='form', submit=__(u"Submit"), ca
     for field in form:
         if isinstance(field.widget, wtforms.widgets.FileInput):
             multipart = True
-    if form.errors:
-        # TODO: Why is this 200?
-        code = 200  # 400
-    else:
-        code = 200
     if not with_chrome:
         template = THEME_FILES[current_app.config['theme']]['autoform_xhr.html.jinja2']
         return render_template(template, form=form, title=title,
@@ -39,7 +33,7 @@ def render_form(form, title, message='', formid='form', submit=__(u"Submit"), ca
         template = THEME_FILES[current_app.config['theme']]['autoform.html.jinja2']
     return make_response(render_template(template, form=form, title=title,
         message=message, formid=formid, submit=submit,
-        cancel_url=cancel_url, ajax=ajax, multipart=multipart, error_template=error_template), code)
+        cancel_url=cancel_url, ajax=ajax, multipart=multipart, error_template=error_template), 200)
 
 
 def render_message(title, message, code=200):
@@ -58,26 +52,18 @@ def render_redirect(url, code=302):
         return redirect(url, code=code)
 
 
-def render_delete_sqla(obj, db, title, message, success=u'', next=None, cancel_url=None, with_response=True):
+def render_delete_sqla(obj, db, title, message, success=u'', next=None, cancel_url=None):
     if not obj:
         abort(404)
     form = ConfirmDeleteForm()
     if request.method in ('POST', 'DELETE') and form.validate():
-        print ('form', request.form)
         if 'delete' in request.form or request.method == 'DELETE':
             db.session.delete(obj)
             db.session.commit()
             if success:
                 flash(success, 'success')
-            if request.is_xhr:
-                return make_response(jsonify(status='ok', doc=_(u"Deleted"), result={'url': next or url_for('index')}), 303)
             return render_redirect(next or url_for('index'), code=303)
         else:
-            if request.is_xhr:
-                return make_response(jsonify(status='ok', doc=_(u"Cancel"), result={'url': cancel_url or next or url_for('index')}), 303)
             return render_redirect(cancel_url or next or url_for('index'), code=303)
-    if with_response:
-        template = THEME_FILES[current_app.config['theme']]['delete.html.jinja2']
-        return make_response(render_template(template, form=form, title=title, message=message))
-    template = THEME_FILES[current_app.config['theme']]['delete_template.html.jinja2']
-    return render_template(template, form=form, title=title, message=message)
+    template = THEME_FILES[current_app.config['theme']]['delete.html.jinja2']
+    return make_response(render_template(template, form=form, title=title, message=message))
