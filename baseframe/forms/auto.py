94b4303461f6014cebc4d4be6e250d5831e288dc
Vidya Ramakrishnan <vidya@hasgeek.in>
2018-01-30 15:06:50 +0530

    Update ref_id assignment

--- baseframe/forms/auto.py
+++ baseframe/forms/auto.py
@@ -18,12 +18,9 @@ class ConfirmDeleteForm(Form):
     cancel = SubmitField(__(u"Cancel"))
 
 
-def render_form(form, title, message='', formid='', submit=__(u"Submit"), cancel_url=None, ajax=False, with_chrome=True):
+def render_form(form, title, message='', formid=None, submit=__(u"Submit"), cancel_url=None, ajax=False, with_chrome=True):
     multipart = False
-    if not formid:
-        ref_id = 'form-' + buid()
-    else:
-        ref_id = 'form-' + formid
+    ref_id = 'form-' + (formid or buid())
     for field in form:
         if isinstance(field.widget, wtforms.widgets.FileInput):
             multipart = True
