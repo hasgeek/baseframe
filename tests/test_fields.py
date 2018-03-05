b247d1193892f08004d0cfffcf993d8e67bf3a32
Bibhas <iambibhas@gmail.com>
2018-03-05 15:47:16 +0530

    removed unnecessary imports

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from coaster.sqlalchemy import BaseMixin
 from coaster.utils import LabeledEnum
-from coaster.db import db
-from sqlalchemy import Column, Unicode
 from baseframe import __
 from baseframe import baseframe
 import baseframe.forms as forms
-import baseframe.forms.sqlalchemy as forms_sqlachemy
 from .fixtures import app1 as app
 
 
