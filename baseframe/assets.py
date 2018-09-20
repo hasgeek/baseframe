ff17c25ff978aee8e379e5de4b53c061bd56e22e
Vidya Ramakrishnan <vidya@hasgeek.in>
2018-09-20 22:34:03 +0530

    Add Font-Awesome v4.7.0

--- baseframe/assets.py
+++ baseframe/assets.py
@@ -203,6 +203,7 @@ assets['marked.js'][Version('0.3.0')] = 'baseframe/js/marked.js'
 # Font Awesome
 assets['fontawesome.css'][Version('3.1.0')] = 'baseframe/css/fontawesome-3.1.css'
 assets['fontawesome.css'][Version('4.4.0')] = 'baseframe/css/fontawesome-4.4.css'
+assets['fontawesome.css'][Version('4.7.0')] = 'baseframe/css/fontawesome-4.7.css'
 
 # Social icons
 assets['rrssb.js'][Version('1.8.5')] = 'baseframe/js/rrssb.js'
