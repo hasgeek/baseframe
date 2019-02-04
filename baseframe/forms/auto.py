0969d582c8023d6e0d57e2663adffa32ad584139
Vidya Ramakrishnan <vidya@hasgeek.in>
2019-02-04 19:59:40 +0530

    Add support for autosave(wip)

--- baseframe/forms/auto.py
+++ baseframe/forms/auto.py
@@ -18,7 +18,7 @@ class ConfirmDeleteForm(Form):
     cancel = SubmitField(__(u"Cancel"))
 
 
-def render_form(form, title, message='', formid=None, submit=__(u"Submit"), cancel_url=None, ajax=False, with_chrome=True):
+def render_form(form, title, message='', formid=None, submit=__(u"Submit"), cancel_url=None, ajax=False, with_chrome=True, autosave=False):
     multipart = False
     ref_id = 'form-' + (formid or buid())
     for field in form:
@@ -28,14 +28,14 @@ def render_form(form, title, message='', formid=None, submit=__(u"Submit"), canc
         template = THEME_FILES[current_app.config['theme']]['ajaxform.html.jinja2']
         return render_template(template, form=form, title=title,
             message=message, formid=formid, ref_id=ref_id, submit=submit,
-            cancel_url=cancel_url, ajax=ajax, multipart=multipart, with_chrome=with_chrome)
+            cancel_url=cancel_url, ajax=ajax, multipart=multipart, with_chrome=with_chrome, autosave=autosave)
     if request.is_xhr and ajax:
         template = THEME_FILES[current_app.config['theme']]['ajaxform.html.jinja2']
     else:
         template = THEME_FILES[current_app.config['theme']]['autoform.html.jinja2']
     return make_response(render_template(template, form=form, title=title,
         message=message, formid=formid, ref_id=ref_id, submit=submit,
-        cancel_url=cancel_url, ajax=ajax, multipart=multipart), 200)
+        cancel_url=cancel_url, ajax=ajax, multipart=multipart, autosave=autosave), 200)
 
 
 def render_message(title, message, code=200):
