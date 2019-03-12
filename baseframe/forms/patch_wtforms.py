1a4cbbdcf1a89a2563abf73019ae4cc2385ed955
Kiran Jonnalagadda <kiran@hasgeek.com>
2019-03-12 14:05:35 +0530

    Remove commented out patch

--- baseframe/forms/patch_wtforms.py
+++ baseframe/forms/patch_wtforms.py
@@ -41,20 +41,3 @@ def _patch_wtforms_field_init():
 
 _patch_wtforms_field_init()
 del _patch_wtforms_field_init
-
-
-# Borrowed from https://github.com/indico/indico/commit/c79c562866e5efdbeb5a3101cccc97df57906f76
-# monkeypatch for https://github.com/wtforms/wtforms/issues/373
-# def _patch_wtforms_sqlalchemy():
-#     from wtforms_sqlalchemy import fields
-#     from sqlalchemy.orm.util import identity_key
-
-#     def get_pk_from_identity(obj):
-#         key = identity_key(instance=obj)[1]
-#         return u':'.join(map(unicode, key))
-
-#     fields.get_pk_from_identity = get_pk_from_identity
-
-
-# _patch_wtforms_sqlalchemy()
-# del _patch_wtforms_sqlalchemy
