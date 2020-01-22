c3239612ccdb59e9ff5d0622e33f2dccd972e225
Karan Saini <37518297+qurbat@users.noreply.github.com>
2020-01-22 15:48:40 +0530

    Update validators.py

--- baseframe/forms/validators.py
+++ baseframe/forms/validators.py
@@ -302,17 +302,16 @@ class NotEqualTo(_Comparison):
 
 class IsEmoji(object):
     """
-    Validate that field.data is either empty or contains an emoji
-    through use of the ``emoji `` package.
+    Validate field to contain a single emoji
 
     :param message:
         Error message to raise in case of a validation error.
     """
     def __init__(self, message=None):
-        self.message = message or _(u"This is not a valid emoji.")
-        
+        self.message = message or _(u'This is not a valid emoji.')
+
     def __call__(self, form, field):
-        if field.data and field.data not in emoji.UNICODE_EMOJI:
+        if field.data not in emoji.UNICODE_EMOJI:
             raise ValidationError(self.message)
                 
 class IsPublicEmailDomain(object):
