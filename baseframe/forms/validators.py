fdd9b26ac9697467fb3b65d749534de59b68fde8
Karan Saini <37518297+qurbat@users.noreply.github.com>
2020-01-23 11:40:36 +0530

    Update validators.py

--- baseframe/forms/validators.py
+++ baseframe/forms/validators.py
@@ -308,7 +308,7 @@ class IsEmoji(object):
         Error message to raise in case of a validation error.
     """
     def __init__(self, message=None):
-        self.message = message or _(u'This is not a valid emoji.')
+        self.message = message or _(u"This is not a valid emoji.")
 
     def __call__(self, form, field):
         if field.data not in emoji.UNICODE_EMOJI:
