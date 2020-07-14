fd43779dd9f380093994531d05c592cbb0fa09c7
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-07-14 14:52:16 +0530

    Update to new logo

--- baseframe/static/js/baseframe-material.js
+++ baseframe/static/js/baseframe-material.js
@@ -321,6 +321,7 @@ window.Baseframe.Forms = {
       .find('button[type="submit"]')
       .click(function (event) {
         event.preventDefault();
+        console.log('handleFormSubmit');
         $.ajax({
           url: url,
           type: 'POST',
