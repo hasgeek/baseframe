b3464519173a96335f76b9c418f64639fb08a6a1
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-07-31 04:57:00 +0530

    Rename HasGeek to Hasgeek (#311)

--- baseframe/forms/validators.py
+++ baseframe/forms/validators.py
@@ -429,7 +429,7 @@ class ValidUrl(object):
     """
 
     user_agent = (
-        "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 HasGeek/linkchecker"
+        "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Hasgeek/linkchecker"
     )
 
     default_message = __("The URL “{url}” is not valid or is currently inaccessible")
--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'baseframe'
-copyright = '2012-20, HasGeek'
+copyright = '2012-20, Hasgeek'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -192,7 +192,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ('index', 'baseframe.tex', 'baseframe Documentation', 'HasGeek', 'manual')
+    ('index', 'baseframe.tex', 'baseframe Documentation', 'Hasgeek', 'manual')
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,7 +220,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [('index', 'baseframe', 'baseframe Documentation', ['HasGeek'], 1)]
+man_pages = [('index', 'baseframe', 'baseframe Documentation', ['Hasgeek'], 1)]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -236,7 +236,7 @@ texinfo_documents = [
         'index',
         'baseframe',
         'baseframe Documentation',
-        'HasGeek',
+        'Hasgeek',
         'baseframe',
         'One line description of project.',
         'Miscellaneous',
