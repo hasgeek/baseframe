c20543d102f83a2be2ce492bd44752c6a6e85ca7
Bibhas <github@bibhasdn.com>
2020-09-04 23:23:49 +0530

    importing newrelic config file if it exists

--- baseframe/__init__.py
+++ baseframe/__init__.py
@@ -404,6 +404,15 @@ class BaseframeBlueprint(Blueprint):
         )
         app.config['RECAPTCHA_DATA_ATTRS'].setdefault('size', 'invisible')
 
+        try:
+            import newrelic.agent
+            import os.path
+
+            if os.path.isfile('newrelic.ini'):
+                newrelic.agent.initialize('newrelic.ini')
+        except ImportError:
+            pass
+
     def register(self, app, options, first_registration=False):
         """
         Called by :meth:`Flask.register_blueprint` to register all views
