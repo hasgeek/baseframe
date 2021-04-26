d8555c74e29a147da667441ca9c21373b5b029a5
Vidya Ramakrishnan <vidya@hasgeek.in>
2021-04-26 15:53:07 +0530

    Open  abbr tooltip on the  right

--- baseframe/static/js/baseframe-material.js
+++ baseframe/static/js/baseframe-material.js
@@ -151,6 +151,12 @@ function activate_geoname_autocomplete(
 function activateZoomPopup() {
   if ($('.markdown').length > 0) {
     $('body').append('<div class="markdown-modal markdown"></div>');
+
+    $('abbr').each(function () {
+      if ($(this).offset().left > $(window).width() * 0.7) {
+        $(this).addClass('tooltip-right');
+      }
+    });
   }
 
   $('body').on('click', '.markdown table, .markdown img', function (event) {
