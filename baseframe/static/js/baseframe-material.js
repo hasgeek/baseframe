bb6cb45f747f741e548439189c41908fb6955376
Vidya Ramakrishnan <vidya@hasgeek.in>
2021-05-14 19:16:47 +0530

    On opening modal, trap tabbing inside the modal

--- baseframe/static/js/baseframe-material.js
+++ baseframe/static/js/baseframe-material.js
@@ -166,6 +166,44 @@ function activateZoomPopup() {
   });
 }
 
+function addFocusOnModalShow() {
+  var focussedElem;
+  $('body').on($.modal.OPEN, '.modal', function () {
+    focussedElem = document.activeElement;
+    trapFocusWithinModal(this);
+  });
+
+  $('body').on($.modal.CLOSE, '.modal', function () {
+    focussedElem.focus();
+  });
+}
+
+function trapFocusWithinModal(modal) {
+  var $this = $(modal);
+  var focusableElems =
+    'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]';
+  var children = $this.find('*');
+  var focusableItems = children.filter(focusableElems).filter(':visible');
+  var numberOfFocusableItems = focusableItems.length;
+  var focusedItem, focusedItemIndex;
+  $this.click();
+  $this.find('.modal__close').focus();
+
+  $this.on('keydown', function (event) {
+    if (event.keyCode != 9) return;
+    focusedItem = $(document.activeElement);
+    focusedItemIndex = focusableItems.index(focusedItem);
+    if (!event.shiftKey && focusedItemIndex == numberOfFocusableItems - 1) {
+      focusableItems.get(0).focus();
+      event.preventDefault();
+    }
+    if (event.shiftKey && focusedItemIndex == 0) {
+      focusableItems.get(numberOfFocusableItems - 1).focus();
+      event.preventDefault();
+    }
+  });
+}
+
 $(function () {
   // activate all widgets
   activate_widgets();
@@ -289,10 +327,10 @@ window.Baseframe.Forms = {
      'errors' is the WTForm validation errors expected in the following format
       {
         "title": [
-          "This field is required"
+          "This field is required."
         ]
         "email": [
-          "Not a valid email"
+          "Not a valid email."
         ]
       }
     For each error, a 'p' tag is created if not present and
@@ -560,4 +598,5 @@ $(function () {
   });
 
   activateZoomPopup();
+  addFocusOnModalShow();
 });
